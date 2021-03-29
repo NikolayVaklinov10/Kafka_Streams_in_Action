22c214820ae6c01e113e2f54923923b595ac7f91
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-03-29 20:28:01 +0100

    the purchase key file

---
+++ kafka-produders-and-consumers/src/main/java/com/github/nikolayvaklinov/kafka/model/PurchaseKey.java
@@ -0,0 +1,38 @@
+package com.github.nikolayvaklinov.kafka.model;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class PurchaseKey {
+
+    private String customerId;
+    private Date transactionDate;
+
+    public PurchaseKey(String customerId, Date transactionDate) {
+        this.customerId = customerId;
+        this.transactionDate = transactionDate;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public Date getTransactionDate() {
+        return transactionDate;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PurchaseKey)) return false;
+        PurchaseKey that = (PurchaseKey) o;
+        return Objects.equals(customerId, that.customerId) &&
+                Objects.equals(transactionDate, that.transactionDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(customerId, transactionDate);
+    }
+}
