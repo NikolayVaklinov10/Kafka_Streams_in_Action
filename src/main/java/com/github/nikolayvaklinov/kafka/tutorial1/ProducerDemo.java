b5ff96a3cb5bbdf3bd3af5df449cfb0f47df502b
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-03-29 13:34:32 +0100

    checking whether java is working properly

---
+++ src/main/java/com/github/nikolayvaklinov/kafka/tutorial1/ProducerDemo.java
@@ -0,0 +1,7 @@
+package com.github.nikolayvaklinov.kafka.tutorial1;
+
+public class ProducerDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello, world. Java is working");
+    }
+}
