749fedf41052800763eb6a42b5441bdc9c91d020
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-03-31 14:41:29 +0100

    this interface is using to detect potential fraud

---
+++ developing-kafka-streams/src/main/java/com/github/nikolayvaklinov/online/store/yellingApp/producer/service/SecurityDBService.java
@@ -0,0 +1,10 @@
+package com.github.nikolayvaklinov.online.store.yellingApp.producer.service;
+
+import java.util.Date;
+
+public interface SecurityDBService {
+
+    static void saveRecord(Date date, String employeeId, String item) {
+        System.out.println("Warning!! Found potential problem !! Saving transaction on "+date+" for "+employeeId+" item "+ item);
+    }
+}
