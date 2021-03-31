3f7d98f4297ba01fbe56ef303c1cace733c21fb1
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-03-31 13:05:52 +0100

    properties config plus the kafka streams bit

--- developing-kafka-streams/src/main/java/com/github/nikolayvaklinov/online/store/yellingApp/ZMartKafkaStreamApp.java
+++ developing-kafka-streams/src/main/java/com/github/nikolayvaklinov/online/store/yellingApp/ZMartKafkaStreamApp.java
@@ -5,14 +5,17 @@ import com.github.nikolayvaklinov.online.store.yellingApp.producer.model.Purchas
 import com.github.nikolayvaklinov.online.store.yellingApp.producer.model.RewardAccumulator;
 import com.github.nikolayvaklinov.online.store.yellingApp.producer.util.MockDataProducer;
 import com.github.nikolayvaklinov.online.store.yellingApp.producer.util.serde.StreamsSerdes;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Printed;
 import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.processor.WallclockTimestampExtractor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -62,7 +65,21 @@ public class ZMartKafkaStreamApp {
         // used only to produce data for this application, not typical usage
         MockDataProducer.producePurchaseData();
 
+        KafkaStreams kafkaStreams = new KafkaStreams(builder.build(), getProperties());
 
+
+
+    }
+
+    private static Properties getProperties() {
+        Properties props = new Properties();
+        props.put(StreamsConfig.CLIENT_ID_CONFIG, "FirstZmart-Kafka-Streams-Client");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG,"zmart-purchases");
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "FirstZmart-Kafka-Streams-App");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(StreamsConfig.REPLICATION_FACTOR_CONFIG, 1);
+        props.put(StreamsConfig.DEFAULT_TIMESTAMP_EXTRACTOR_CLASS_CONFIG, WallclockTimestampExtractor.class);
+        return props;
 
 
     }
