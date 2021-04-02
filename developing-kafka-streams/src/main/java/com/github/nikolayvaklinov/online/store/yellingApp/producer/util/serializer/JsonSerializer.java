b406f38cd512848ae1599eed8baf8d505cdc7b86
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-04-02 19:44:07 +0100

    the json serializer with google's Gson library

---
+++ developing-kafka-streams/src/main/java/com/github/nikolayvaklinov/online/store/yellingApp/producer/util/serializer/JsonSerializer.java
@@ -0,0 +1,35 @@
+package com.github.nikolayvaklinov.online.store.yellingApp.producer.util.serializer;
+
+import com.github.nikolayvaklinov.online.store.yellingApp.producer.collectors.FixedSizePriorityQueue;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.apache.kafka.common.serialization.Serializer;
+
+import java.nio.charset.Charset;
+import java.util.Map;
+
+public class JsonSerializer<T> implements Serializer<T> {
+
+    private Gson gson;
+
+    public JsonSerializer() {
+        GsonBuilder builder = new GsonBuilder();
+        builder.registerTypeAdapter(FixedSizePriorityQueue.class, new FixedSizePriorityQueueAdapter().nullSafe());
+        gson = builder.create();
+    }
+
+    @Override
+    public void configure(Map<String, ?> map, boolean b) {
+
+    }
+
+    @Override
+    public byte[] serialize(String topic, T t) {
+        return gson.toJson(t).getBytes(Charset.forName("UTF-8"));
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
