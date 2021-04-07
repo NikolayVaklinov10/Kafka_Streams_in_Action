4cdf993e745b01c8114051b28883be9ec4a6aa09
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-04-07 11:09:20 +0100

    enumeration for converting the currency of the beer purchase

---
+++ developing-kafka-streams/src/main/java/com/github/nikolayvaklinov/online/store/yellingApp/producer/model/Currency.java
@@ -0,0 +1,24 @@
+package com.github.nikolayvaklinov.online.store.yellingApp.producer.model;
+
+public enum Currency {
+
+    EURO(1.09),
+
+    POUNDS(1.2929),
+
+    DOLLARS(1.0);
+
+
+    Currency(double conversionRate) {
+
+        this.conversionRate = conversionRate;
+    }
+
+    private double conversionRate;
+
+
+    public double convertToDollars(double internationalAmount) {
+
+        return internationalAmount/conversionRate;
+    }
+}
