94b14e31f11c3154c70ed1b3a61526051206ea54
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-04-13 14:54:45 +0100

    the producer interceptor bit

---
+++ developing-kafka-streams/src/main/java/com/github/nikolayvaklinov/online/store/Monitoring/interceptors/ZMartProducerInterceptor.java
@@ -0,0 +1,42 @@
+package com.github.nikolayvaklinov.online.store.Monitoring.interceptors;
+
+import org.apache.kafka.clients.producer.ProducerInterceptor;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+public class ZMartProducerInterceptor implements ProducerInterceptor<Object, Object> {
+
+
+    private static final Logger LOG = LoggerFactory.getLogger(ZMartProducerInterceptor.class);
+
+    @Override
+    public ProducerRecord<Object, Object> onSend(ProducerRecord<Object, Object> record) {
+
+        LOG.info("ProducerRecord being sent out {} ", record);
+
+        return record;
+    }
+
+    @Override
+    public void onAcknowledgement(RecordMetadata metadata, Exception exception) {
+        if (exception != null) {
+            LOG.warn("Exception encountered producing record {}", exception);
+        } else {
+            LOG.info("record has been acknowledged {} ", metadata);
+        }
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public void configure(Map<String, ?> configs) {
+
+    }
+}
